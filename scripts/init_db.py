b4e53429028ea2ace419007b4e7e293f635c11b4
Haohan Yang <yanghaohanmail@gmail.com>
2023-04-25 13:30:00 +0200

    add python init db script

---
+++ scripts/init_db.py
@@ -0,0 +1,27 @@
+from os import getenv
+import pymssql
+
+server = getenv("DEV_DATASOURCE_SERVER")
+username = getenv("DEV_DATASOURCE_USERNAME")
+password = getenv("DEV_DATASOURCE_PASSWORD")
+database = "tempdb"
+
+schema = "app"
+
+conn = pymssql.connect(server, username, password, database)
+cursor = conn.cursor(as_dict=True)
+
+cursor.execute("CREATE SCHEMA " + schema)
+cursor.execute("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA")
+
+schema_created = False
+
+row = cursor.fetchone()
+while row:
+    if row["SCHEMA_NAME"] == schema:
+        schema_created = True
+    row = cursor.fetchone()
+conn.close()
+
+if not schema_created:
+    print(SystemError, "Failed to create schema app")
